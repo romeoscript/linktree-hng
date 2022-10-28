009ec06cf3ccfcf1335bf3490adfcc0508ef5458
Romeoscript <romeobourne211@gmail.com>
2022-10-28 04:56:00 +0100

    first commit

---
+++ src/Links.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Links = () => {
+  return (
+    <div className='links'>
+      <a href="">Twitter Link</a>
+      <a href="">Zuri Team</a>
+      <a href="">Zuri Books</a>
+      <a href="">Python Books</a>
+      <a href="">Background Check for Coders</a>
+      <a href="">Design Books</a>
+     
+    </div>
+  )
+}
+
+export default Links
