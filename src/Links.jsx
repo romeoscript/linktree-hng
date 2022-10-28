9bee71dd00bee22225967ade7a8025bf8a6b576c
Romeoscript <romeobourne211@gmail.com>
2022-10-28 05:07:34 +0100

    the zuri and i4g socials added

--- src/Links.jsx
+++ src/Links.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import github from './assets/Social icon.png'
-import slack from './assets/slack.png'
+import React from "react";
+import github from "./assets/Social icon.png";
+import slack from "./assets/slack.png";
+import zuri from "./assets/Zuri.Internship_Logo.png";
+import i4g from "./assets/I4G.png";
 
 const Links = () => {
   return (
-   <>
-    <div className='links'>
-      <a href="">Twitter Link</a>
-      <a href="">Zuri Team</a>
-      <a href="">Zuri Books</a>
-      <a href="">Python Books</a>
-      <a href="">Background Check for Coders</a>
-      <a href="">Design Books</a>
-     
-    </div>
-    <div className="social__links">
+    <>
+      <div className="links">
+        <a href="">Twitter Link</a>
+        <a href="">Zuri Team</a>
+        <a href="">Zuri Books</a>
+        <a href="">Python Books</a>
+        <a href="">Background Check for Coders</a>
+        <a href="">Design Books</a>
+      </div>
+      <div className="social__links">
         <img src={slack} alt="" />
         <img src={github} alt="" />
-    </div>
-   </>
-  )
-}
+      </div>
+      <div className="footer">
+        <img src={zuri} alt="" />
+        <p>HNG Internship 9 Frontend Task</p>
+        <img src={i4g} alt="" />
+      </div>
+    </>
+  );
+};
 
-export default Links
+export default Links;
