d0202c45f69541a26f8e2682e1c843073120fe3e
Romeoscript <romeobourne211@gmail.com>
2022-11-04 02:10:14 +0100

    feat: checkbox to the contact form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './contact.css'
 
 const Contact = () => {
+    const name = `{name}`
   return (
     <div className='contact'>
       <h2>Contact Me</h2>
@@ -16,12 +17,15 @@ const Contact = () => {
                 <label htmlFor="Last name">Last name</label> <br />
                 <input type="text" placeholder='Enter your Last name'/>
             </div>
-            <label htmlFor="email">Email</label> <br />
+            
+        </main>
+        <label htmlFor="email">Email</label> <br />
             <input type="email" placeholder='yourname@email.com' />
             <label htmlFor="message">Message</label><br />
             <textarea name="" id="" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+
+            <input type="checkbox" /> <span>You agree to providing your data to {name} who may contact you.</span>
             <input type="submit" value="Send message" />
-        </main>
       </form>
     </div>
   )
